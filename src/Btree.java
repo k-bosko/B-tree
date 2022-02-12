662e77bb8cd1aaa2a760c7622b6bdabb17bc7c8f
Katerina Bosko <katerinabosko@gmail.com>
2022-02-12 12:38:51 -0800

    refactored code - moved functions that operate on node to Node class

--- src/Btree.java
+++ src/Btree.java
@@ -9,11 +9,13 @@
  *   2. Lookup
  *   3. Display
  *
+ * B-tree data structure consists of array of values and array of children that are integer pointers
+ * to nodes. Nodes array tracks all nodes inserted in B-tree.
+ *
  * The implementation supports both odd and even number of node size,
  * i.e. maximum number of values that are allowed per node.
- *
- * Limitations: inserted value cannot be 0 because the data structure used is simple array
- * that is initialized implicitly with 0s.
+ * Note: Mid position for even node size is determined by taking the first value
+ * to the left from the middle after splitting array in half.
  *
  * The implementation relies on left biasing after split.
  * This means that during redistribution between the current node and new node
@@ -25,12 +27,16 @@
    which allows to easily promote the 'invisible' middle value into parent node at later stage.
    Such an implementation is more efficient because with right biasing
    one would need to implement more steps -> pop the middle value from the new node when it's full.
+
+ * Limitations:
+ * 1) this implementation doesn't store a data pointer to a physical location on disk
+ * (like it would be normally done in DBMS)
+ * 2) inserted value cannot be 0 because the data structure used is simple array
+ * that is initialized implicitly with 0s.
  */
 
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
 
 final class Btree {
 
@@ -52,9 +58,6 @@ final class Btree {
   /* Size of Node */
   private final int nodeSize;
 
-  /* middle position in an array of nodeSize, differs if nodeSize is odd or even  */
-  private final int mid;
-
   /**
    * B tree Constructor.
    */
@@ -63,7 +66,6 @@ final class Btree {
     root = createNode();
     nodes[root].isLeaf = false;
     nodes[root].children[0] = createLeaf();
-    mid = ((nodeSize + 1) % 2 == 0)? (nodeSize + 1)/2 - 1:  Math.floorDiv(nodeSize + 1, 2);
   }
 
   /**
@@ -74,10 +76,7 @@ final class Btree {
     root = createNode();
     nodes[root].isLeaf = false;
     nodes[root].children[0] = createLeaf();
-    mid = ((nodeSize + 1) % 2 == 0)? (nodeSize + 1)/2 - 1:  Math.floorDiv(nodeSize + 1, 2);
   }
-
-  /*********** B tree functions for Public ******************/
 
   /**
    * Lookup(int value)
@@ -110,7 +109,7 @@ final class Btree {
    */
   public void displayTree(){
     System.out.println("********************");
-    System.out.println("\nDisplaying the B-tree");
+    System.out.println("Displaying the B-tree");
     System.out.println("node size: " + nodeSize);
     System.out.println("num values: " + cntValues);
     System.out.println("********************");
@@ -125,8 +124,6 @@ final class Btree {
   public void display(int nodePtr){
     displayNode(nodePtr, 0);
   }
-
-  /*********** B-tree functions for Internal  ******************/
 
   /**
    * nodeLookup(int value, int pointer)
@@ -148,11 +145,27 @@ final class Btree {
       //go down the tree
       else {
         //find the child node for the current node
-        int childPtr = findChild(curr, value);
+        int childPtr = curr.findChild(value);
         //start recursion to go down the tree
         return nodeLookup(value, childPtr);
       }
     }
+  }
+
+  /**
+   * found(int value)
+   *   helper function to check if the value is stored in the current node
+   *   - true if found
+   *   - false if not found
+   */
+  private boolean found(Node curr, int value){
+    // check  if value exists
+    for (int i = 0; i < nodeSize; i++) {
+      if (curr.values[i] == value) {
+        return true; // exit of found
+      }
+    }
+    return false;
   }
 
   /**
@@ -194,7 +207,7 @@ final class Btree {
     if (curr.isLeaf) {
       //if there is space -> insert value into leaf node in sorted order
       if (curr.size < nodeSize) {
-        insert(curr, value);
+        curr.insert(value);
         return -1;
       }
       //there is no space
@@ -204,14 +217,14 @@ final class Btree {
         //checkout the newly created node based on its pointer
         Node newLeaf = nodes[newLeafPtr];
         //redistribute values in the current and new node
-        redistributeValues(curr, value, newLeaf);
+        curr.redistributeValues(value, newLeaf);
         return newLeafPtr;
       }
     }
     //current node is not leaf, i.e. inner node
     else {
       //find the correct child pointer for the current node
-      int childPtr = findChild(curr, value);
+      int childPtr = curr.findChild(value);
       //checkout child node based on its pointer
       Node child = nodes[childPtr];
       //insert the value into child
@@ -230,7 +243,7 @@ final class Btree {
         // because current node is not leaf -> insert will sort children as well
         // insert returns position at which midValue was inserted
         // needed in next step to insert newChildPtr at correct position among children
-        int insertPos = insert(curr, midValue);
+        int insertPos = curr.insert(midValue);
         // link current node with new child
         curr.children[insertPos + 1] = newChildPtr;
         return -1;
@@ -241,11 +254,12 @@ final class Btree {
         int newNodePtr = createNode();
         //checkout new node
         Node newNode = nodes[newNodePtr];
-        //find new middle value to be promoted
+        //find new middle value to be promoted -> stored 'invisible' at curr.size position in child
         int midVal = child.values[child.size];
-        //redistribute values between current node and new node & return position at which value was inserted
-        int insertPos = redistributeValues(curr, midVal, newNode);
-        redistributeChildren(curr, newChildPtr, newNode, insertPos);
+        //redistribute values between current node and new node
+        // & return position at which the value was inserted
+        int insertPos = curr.redistributeValues(midVal, newNode);
+        curr.redistributeChildren(newChildPtr, newNode, insertPos);
 
         //if current node is not root
         if (curr != nodes[root]){
@@ -270,164 +284,6 @@ final class Btree {
       }
     }
   }
-
-  /**
-   * found(int value)
-   *   helper function to check if the value is stored in the current node
-   *   - true if found
-   *   - false if not found
-   */
-  private boolean found(Node curr, int value){
-    // check  if value exists
-    for (int i = 0; i < nodeSize; i++) {
-      if (curr.values[i] == value) {
-        return true; // exit of found
-      }
-    }
-    return false;
-  }
-
-  /**
-   * insert(int value)
-   *   helper function to insert the value at correct position
-   *   in array of values that has increasing order
-   *   TODO add desc for child pointers
-   */
-  private int insert(Node curr, int value){
-    int i = 0;
-    //find where to insert by moving other values
-    for (i = curr.size; i > 0 && value < curr.values[i - 1]; i--){
-      curr.values[i] = curr.values[i - 1];
-      curr.children[i + 1] = curr.children[i];
-    }
-    //insert key
-    curr.children[i + 1] = curr.children[i];
-    curr.values[i] = value;
-    curr.size++;
-    return i;
-  }
-
-  /**
-   * findChild(int value)
-   *   helper function to find the correct child of the current node
-   *   where value will be processed
-   *   returns pointer to a child
-   */
-  private int findChild(Node curr, int value){
-    int i = 0;
-    //find the correct index by comparing value to values in the current node
-    while (i < curr.size && curr.values[i] < value){
-      i += 1;
-    }
-    return curr.children[i];
-  }
-
-  /**
-   * redistribute(int value, Node newNode)
-   *   helper function to redistribute values between current node
-   *   and newly created node based on mid position
-   *   1. the values to be redistributed include the value to be inserted and are sorted
-   *   2. the values are sorted to find the middle value that will be later promoted into parent
-   *   3. values are redistributed based on left bias:
-   *      - all values up to mid value (inclusive) stay in the current node
-   *      - all values after mid value go to new node
-   *   NOTE: mid position for even node size is determined by taking the first value
-   *   to the left from the middle after splitting array in half
-   *
-   */
-
-  private int redistributeValues(Node curr, int value, Node newNode){
-    //create temporary array that will hold all values from current node and value to be added
-    int[] arr = new int[nodeSize + 1];
-
-    //copy over all values in the current node to temporary array
-    int insertPos = 0;
-    int i = 0;
-    for (i = 0; i < curr.size; i++){
-      if (curr.values[i] < value){
-        arr[i] = curr.values[i];
-      }
-      else {
-        arr[i] = value;
-        insertPos = i;
-        break;
-      }
-    }
-    if (i < curr.size){
-      for (int j = i; j < curr.size; j++){
-        arr[j + 1] = curr.values[j];
-      }
-    }
-    else {
-      arr[i] = value;
-      insertPos = i;
-    }
-
-    //redistribute into current node -> all values up to and including mid value
-    int newSize = 0;
-    for (int k = 0; k < mid + 1; k++){
-      curr.values[k] = arr[k];
-      newSize++;
-    }
-    // don't overwrite no longer needed values, instead control with size
-    // -1 so not to count mid (to make it 'invisible')
-    curr.size = newSize - 1;
-
-    int j = 0;
-    //redistribute into new node -> all values after mid
-    for (int m = mid + 1; m < arr.length; m++){
-      newNode.values[j] = arr[m];
-      newNode.size++;
-      j++;
-    }
-    return insertPos;
-  }
-
-  private void redistributeChildren(Node curr, int newChildPtr, Node newNode, int insertPos){
-    int[] arr = new int[nodeSize + 2];
-    int newChildPos = insertPos + 1;
-    int midForChildren = mid + 1;
-    //copy over all children of curr and newChildPtr (at correct position) into temporary array
-    int j = 0;
-    for (int i = 0; i < arr.length; i++){
-      if (i == newChildPos){
-        arr[i] = newChildPtr;
-      }
-      else {
-        arr[i] = curr.children[j];
-        j++;
-      }
-    }
-    int numChildrenToTransfer = arr.length - midForChildren;
-
-    //redistribute children into new node & reset children for current node
-    for (int k = 0; k < numChildrenToTransfer; k++){
-      newNode.children[k] = arr[k + midForChildren];
-      curr.children[k + midForChildren - 1] = 0;
-    }
-    //redistribute children into current node
-    for (int m = 0; m < midForChildren; m++){
-      curr.children[m] = arr[m];
-    }
-  }
-
-  private void sortValuesAndChildren(Node curr, int value){
-    //if midValue is less than the last but one value in current node -> sort
-    if (curr.size > 1 && value < curr.values[curr.size - 2]){
-      int i;
-      int lastChild = curr.children[curr.size];
-      //find where to insert by moving other values
-      for (i = curr.size - 2; i >= 0 && value < curr.values[i]; i--){
-        curr.values[i + 1] = curr.values[i];
-        curr.children[i + 2] = curr.children[i + 1];
-      }
-      //insert key
-      curr.children[i + 2] = lastChild;
-      curr.values[i + 1] = value;
-    }
-  }
-
-  /*********** Functions for accessing node  ******************/
 
   /**
    * createLeaf(): Creates a new leaf node
@@ -469,70 +325,35 @@ final class Btree {
 
   public static void main(String[] args) {
 
-    Btree b = new Btree(5);
+    //controlling variables
     int numValues = 30;
-    int[] randomNumArr = new int[numValues];
+    int nodesize = 5;
+
+    Btree b = new Btree(nodesize);
+
+    ArrayList<Integer> randomNumList = new ArrayList<>();
     System.out.println("Inserting: ");
     for (int i = 0; i < numValues; i++) {
       //generate random number in range 0 to 100
       int randomNum = 1 + (int) (Math.random() * ((100 - 1) + 1));
-      randomNumArr[i] = randomNum;
-      System.out.print(randomNum + ", ");
+      randomNumList.add(randomNum);
+      System.out.print(randomNum + " ");
       b.insert(randomNum);
     }
 
-    int[] nonRepeatingArr = Arrays.stream(randomNumArr).distinct().toArray();
     System.out.println(
         "\n\ntried to insert (potentially with duplicates): " + numValues + " values");
-    System.out.println("must be inserted: " + nonRepeatingArr.length + " values");
     System.out.println("de facto inserted: " + b.getCntValues() + " values");
 
     if (numValues != b.getCntValues()) {
-      List<Integer> randomNumList = Arrays.stream(randomNumArr).boxed()
-          .collect(Collectors.toList());
-
-      System.out.println("\nInserted without duplicates:");
-      randomNumList.stream()
+      System.out.println("\nInserted (without duplicates):");
+      randomNumList
+          .stream()
           .distinct()
           .forEach(e -> System.out.print(e + " "));
-      System.out.println();
+      System.out.println("\n");
     }
     b.displayTree();
-
-//    Btree bt = new Btree(2);
-//    bt.insert(8);
-//    bt.insert(5);
-//    bt.insert(1);
-//    bt.insert(7);
-//    bt.insert(3);
-//    bt.insert(12);
-//    bt.insert(9);
-//    bt.insert(6);
-//    bt.insert(13);
-//    bt.displayTree();
-
-//    Btree b = new Btree(3);
-//    b.insert(20);
-//    b.insert(30);
-//    b.insert(10);
-//    b.insert(40);
-//    b.insert(50);
-//    b.insert(60);
-//    b.insert(70);
-//    b.insert(80);
-//    b.insert(90);
-//    b.insert(100);
-//    b.insert(101);
-//    b.insert(102);
-//    b.insert(103);
-//    b.insert(104);
-//    b.insert(105);
-//    b.insert(106);
-//    b.insert(107);
-//    b.insert(108);
-//    b.insert(109);
-//    b.insert(110);
-//    b.displayTree();
   }
 }
 
--- src/Node.java
+++ src/Node.java
@@ -1,18 +1,19 @@
 /*
+ * CS7280 Special Topics in Database Management
+ * Project 1: B-tree implementation.
+ * Author: Katerina Bosko
+
  * Node data structure.
- *   - This is the simplest structure for nodes used in B-tree
- *   - This will be used for both internal and leaf nodes.
  */
+
 final class Node {
-  /* Node Values (Leaf Values / Key Values for the children nodes).  */
+  /* node values  */
   int[] values;
 
-  /* Node Array, pointing to the children nodes.
-   * This array is not initialized for leaf nodes.
-   */
+  /* node array pointing to the children nodes */
   int[] children;
 
-  /* Number of entries
+  /* number of entries
    * (Rule in B Trees:  d <= size <= 2 * d).
    */
   int size;
@@ -21,4 +22,157 @@ final class Node {
   boolean isLeaf;
 
 
+  /**
+   * insert(int value)
+   *   inserts the value at correct position
+   *   into node's array of values in an increasing order
+   *   sorts node's children accordingly
+   */
+  public int insert(int value){
+    int i;
+    //find where to insert by moving other values
+    for (i = this.size; i > 0 && value < this.values[i - 1]; i--){
+      this.values[i] = this.values[i - 1];
+      //sort children as well (relevant for inner nodes with space)
+      this.children[i + 1] = this.children[i];
+    }
+    //insert value
+    this.values[i] = value;
+    this.size++;
+    // update children after insert (relevant for inner nodes with space)
+    this.children[i + 1] = this.children[i];
+
+    return i;
+  }
+
+  /**
+   * findChild(int value)
+   *   finds the correct child of the current node
+   *   where value will be processed
+   *   returns pointer to a child
+   */
+  public int findChild(int value){
+    int i = 0;
+    //find the correct index by comparing value to values in the current node
+    while (i < this.size && this.values[i] < value){
+      i += 1;
+    }
+    return this.children[i];
+  }
+
+  /**
+   * redistributeValues(int value, Node newNode)
+   *   redistributes values between current node
+   *   and newly created node based on mid position
+   *   1. the values to be redistributed include all values of current node + value to be inserted
+   *      stored into temporary array
+   *   2. the values are sorted to find the middle value that will be later promoted into parent
+   *   3. values are redistributed based on left bias:
+   *      - all values up to mid value (inclusive) stay in the current node
+   *      - all values after mid value go to new node
+   *   NOTE: mid position for even node size is determined by taking the first value
+   *   to the left from the middle after splitting array in half
+   */
+
+  public int redistributeValues(int value, Node newNode){
+    //create temporary array that will hold all values from current node and value to be inserted
+    //because we redistribute only when node is full, this.size = nodeSize
+    int arrLen = this.size + 1;
+    int[] arr = new int[arrLen];
+
+    //calculate mid position of values -> depends on whether array is odd or even
+    int mid = (arrLen % 2 == 0)? arrLen/2 - 1:  Math.floorDiv(arrLen, 2);
+
+    //copy over all values of the current node and value to insert (at correct position)
+    // to temporary array in increasing order
+    int insertPos = 0;
+    int i;
+    for (i = 0; i < this.size; i++){
+      if (this.values[i] < value){
+        arr[i] = this.values[i];
+      }
+      else {
+        arr[i] = value;
+        insertPos = i;
+        break;
+      }
+    }
+    if (i < this.size){
+      for (int j = i; j < this.size; j++){
+        arr[j + 1] = this.values[j];
+      }
+    }
+    else {
+      arr[i] = value;
+      insertPos = i;
+    }
+
+    //redistribute into current node -> all values up to and including mid value
+    int newSize = 0;
+    for (int k = 0; k < mid + 1; k++){
+      this.values[k] = arr[k];
+      newSize++;
+    }
+    // don't overwrite no longer needed values, instead control with size
+    // -1 so not to count mid (to make it 'invisible')
+    this.size = newSize - 1;
+
+    int j = 0;
+    //redistribute into new node -> all values after mid
+    for (int m = mid + 1; m < arr.length; m++){
+      newNode.values[j] = arr[m];
+      newNode.size++;
+      j++;
+    }
+    return insertPos;
+  }
+
+  /**
+   * redistributeChildren(int newChildPtr, Node newNode, int insertPos)
+   *   redistributes children between current node
+   *   and newly created node based on mid position
+   *   1. the children to be redistributed include all children of current node + child to be inserted
+   *   2. all children are copied over into temporary array in special order. The position of new child
+   *   is based on position of the inserted value into parent node during redistribution of values
+   *   3. children are redistributed as following:
+   *      - all children before mid value go to current node
+   *      - all children after mid value go to new node
+   *   NOTE: mid position for even node size is determined by taking the first value
+   *   to the left from the middle after splitting array in half
+   */
+  public void redistributeChildren(int newChildPtr, Node newNode, int insertPos){
+    //create temporary array to hold all children from current node + a child to be inserted (+1)
+    int arrLen = this.children.length + 1;
+    int[] arr = new int[arrLen];
+    // position of new child is 1 greater than insert position of value into node
+    int newChildPos = insertPos + 1;
+
+    // calculate mid position for temporary array of children
+    int mid = ((arrLen + 1) % 2 == 0)? (arrLen + 1)/2 - 1:  Math.floorDiv(arrLen + 1, 2);
+
+    //copy over all children of this node and newChildPtr (at correct position) into temporary array
+    int j = 0;
+    for (int i = 0; i < arr.length; i++){
+      if (i == newChildPos){
+        arr[i] = newChildPtr;
+      }
+      else {
+        arr[i] = this.children[j];
+        j++;
+      }
+    }
+    //all children after mid go to new node
+    int numChildrenToTransfer = arrLen - mid;
+
+    //redistribute children into new node
+    for (int k = 0; k < numChildrenToTransfer; k++){
+      newNode.children[k] = arr[k + mid];
+      //reset children for current node
+      this.children[k + mid - 1] = 0;
+    }
+    //redistribute children into current node
+    for (int m = 0; m < mid; m++){
+      this.children[m] = arr[m];
+    }
+  }
 }
